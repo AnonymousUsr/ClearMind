f16573228869e9c47d273a4d2df3ca8606ad80c1
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2023-09-09 16:25:48 -0700

    navbar done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "18px";
     document.getElementById("navbar-right").style.fontSize = "16px";
   } else {
-    document.getElementById("navbar").style.padding = "5px 20px";
+    document.getElementById("navbar").style.padding = "10px 20px";
     document.getElementById("logo").style.fontSize = "20px";
     document.getElementById("navbar-right").style.fontSize = "18px";
   }
