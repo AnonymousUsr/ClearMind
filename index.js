294598aeeb759b1703884b6a5dedd5b8893e5a3a
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2023-09-10 13:49:22 -0700

    omg logo and better homepage navbar

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "0px 15px";
+    
+    const navbarElements = document.getElementsByClassName("navbar");
+    for (let i = 0; i < navbarElements.length; i++) {
+      navbarElements[i].style.padding = "0px 15px";
+    }
+    // document.getElementById("navbar").style.padding = "0px 15px";
+
     document.getElementById("logo").style.fontSize = "18px";
     document.getElementById("navbar-right").style.fontSize = "16px";
+    document.getElementById("homenavbar").style.backgroundColor = "#402617";
   } else {
-    document.getElementById("navbar").style.padding = "10px 20px";
+
+    const navbarElements = document.getElementsByClassName("navbar");
+    for (let i = 0; i < navbarElements.length; i++) {
+      navbarElements[i].style.padding = "10px 20px";
+    }
+    //document.getElementById("navbar").style.padding = "10px 20px";
+
     document.getElementById("logo").style.fontSize = "20px";
     document.getElementById("navbar-right").style.fontSize = "18px";
+    document.getElementById("homenavbar").style.backgroundColor = "transparent";
   }
 }
 
